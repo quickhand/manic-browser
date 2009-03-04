40a3f033ae18a7903742ed3e402fa36a87e76484
Marc Lajoie <quickhand@openinkpot.org>
2009-03-04 17:48:30 -0500

    removed custom scrollbars, now relying on ewebkit touch-scrolling

--- src/manic.cpp
+++ src/manic.cpp
@@ -42,7 +42,7 @@ using namespace std;
 
 
 Evas *evas;
-Evas_Object *win, *bg, *mainscroll,*pagelayout,*webkitobj,*addressentry_embed;
+Evas_Object *win, *bg, *pagelayout,*webkitobj,*addressentry_embed;
 Etk_Widget *addressentry,*embed;
 /* return value of app - keep it here so we can modify it */
 static int retval = -1;
@@ -84,9 +84,9 @@ const char *get_cur_url()
 static void
 win_del(void *data, Evas_Object *obj, void *event_info)
 {
-   retval = -1;
-   /* cleanly exit */
-   elm_exit();
+    retval = -1;
+    /* cleanly exit */
+    elm_exit();
 }
 
 static void addressentry_activated(void *data)
@@ -113,189 +113,125 @@ static void stop_bt_clicked(void *data, Evas_Object *obj, void *event_info)
 {
     ewk_webview_object_navigation_stop(webkitobj);
 }
-static void content_resize()
-{
-    if(!evas_object_visible_get(webkitobj))
-        return;
-    
-    Evas_Object *webpage=ewk_webview_object_webpage_get(webkitobj);
-    if(webpage==NULL)
-        return;
-    if(!evas_object_visible_get(webpage))
-        return;
-    
-    Evas_Object *frame=ewk_webpage_object_mainframe_get(webpage);
-    if(frame==NULL)
-    {
-        return;
-    }
-    
-    int framew,frameh;
-    ewk_webframe_object_contents_size_get(frame,&framew,&frameh);
-    if(framew>0 && frameh>0)
-        evas_object_resize(pagelayout,framew-1,frameh-1);
-}
 
-static void  mainscroll_resizing(void *data, Evas *e, Evas_Object *obj, void *event_info)
-{
-    content_resize();
-}
-static void  webkitobj_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info)
-{
-    cancelmouseup=0;
-    mouseclicked=1;
-    ewk_event_feed_mouse_down(webkitobj, (Evas_Event_Mouse_Down *)event_info);
-}
-static void  webkitobj_mouse_move(void *data, Evas *e, Evas_Object *obj, void *event_info)
-{
-    if(!mouseclicked)
-        ewk_event_feed_mouse_move(webkitobj, (Evas_Event_Mouse_Move *)event_info);
-    else
-        cancelmouseup=1;
-}
-static void  webkitobj_mouse_up(void *data, Evas *e, Evas_Object *obj, void *event_info)
-{
-    
-    mouseclicked=0;
-    if(!cancelmouseup)
-        ewk_event_feed_mouse_up(webkitobj, (Evas_Event_Mouse_Up *)event_info);
-    cancelmouseup=0;
-    evas_object_focus_set(webkitobj,TRUE);
-}
-static void  webkitobj_key_down(void *data, Evas *e, Evas_Object *obj, void *event_info)
-{
-    ewk_event_feed_key_press(webkitobj, (Evas_Event_Key_Down *)event_info);
-}
 
 static void page_loading(void *data, Evas_Object *obj, void *event_info)
 {
     
-    content_resize();
-}
 
+}
 
 static void page_url_changed(void *data, Evas_Object *obj, void *event_info)
 {
-    //int partx,party,partw,parth;
-    //edje_object_part_text_cursor_geometry_get(mainscroll,"bg",&partx,&party,&partw,&parth);
-    //evas_object_geometry_get(mainscroll,&x,&y,&w,&h);
-    //evas_object_resize(pagelayout,1,1);
-    //elm_entry_entry_set(address_entry,get_cur_url());
-    int scrx,scry,scrw,scrh;
-    evas_object_geometry_get(mainscroll,&scrx,&scry,&scrw,&scrh);
-    Evas_Object *webpage=ewk_webview_object_webpage_get(webkitobj);
-    if(webpage!=NULL)
-        ewk_webpage_object_viewport_size_set(webpage,scrw,scrh);
-    elm_scroller_region_show(mainscroll,0,0,scrx,scrh);
     etk_entry_text_set(ETK_ENTRY(addressentry),get_cur_url());
-    content_resize();
 }
 
-
+static void  webkitobj_mouse_down(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+    evas_object_focus_set(webkitobj,TRUE);
+}
 
 EAPI int
 elm_main(int argc, char **argv)
 {
-   etk_init(argc,argv);
-   Evas_Object *bx, *bx2, *bt,*icon;
-   
-   /* new window - do the usual and give it a name, title and delete handler */
-   win = elm_win_add(NULL, "dialog", ELM_WIN_BASIC);
-   elm_win_title_set(win, "Dialog");
-   evas_object_smart_callback_add(win, "delete-request", win_del, NULL);
-   evas_object_resize(win,winwidth,winheight);
-   evas_object_size_hint_max_set(win,winwidth,winheight);
-   /*get evas*/
-   evas=evas_object_evas_get(win);
-   ewk_init(evas);
-   char *themefile;
-   themefile=get_theme_file();
-   ewk_theme_set(themefile);
-   /* add a standard bg */
-   bg = elm_bg_add(win);
-   /* not not allow bg to expand. let's limit dialog size to contents */
-   evas_object_size_hint_weight_set(bg, 1.0, 1.0);
-   elm_win_resize_object_add(win, bg);
-   evas_object_show(bg);
-
-   /* add a box object - default is vertical. a box holds children in a row,
+    etk_init(argc,argv);
+    Evas_Object *bx, *bx2, *bt,*icon;
+    
+    /* new window - do the usual and give it a name, title and delete handler */
+    win = elm_win_add(NULL, "dialog", ELM_WIN_BASIC);
+    elm_win_title_set(win, "Dialog");
+    evas_object_smart_callback_add(win, "delete-request", win_del, NULL);
+    evas_object_resize(win,winwidth,winheight);
+    evas_object_size_hint_max_set(win,winwidth,winheight);
+    /*get evas*/
+    evas=evas_object_evas_get(win);
+    ewk_init(evas);
+    char *themefile;
+    themefile=get_theme_file();
+    ewk_theme_set(themefile);
+    /* add a standard bg */
+    bg = elm_bg_add(win);
+    /* not not allow bg to expand. let's limit dialog size to contents */
+    evas_object_size_hint_weight_set(bg, 1.0, 1.0);
+    elm_win_resize_object_add(win, bg);
+    evas_object_show(bg);
+    
+    /* add a box object - default is vertical. a box holds children in a row,
     * either horizontally or vertically. nothing more. */
-   bx = elm_box_add(win);
-   evas_object_size_hint_weight_set(bx, 1.0, 1.0);
-   evas_object_size_hint_align_set(bt, -1.0, -1.0);
-   elm_win_resize_object_add(win, bx);
-   evas_object_show(bx);
-   
-   /* add controls box */
-   bx2 = elm_box_add(win);
-   elm_box_horizontal_set(bx2,TRUE);
-   evas_object_size_hint_weight_set(bx2, 1.0, 0.0);
-   //evas_object_size_hint_weight_set(mainscroll, 1.0, 1.0);
-   evas_object_size_hint_align_set(mainscroll, -1.0, -1.0);
-   elm_box_homogenous_set(bx2,FALSE);
-   elm_box_pack_end(bx,bx2);
-   evas_object_show(bx2);
-   
-   /* add buttons to control box */
-   
-   icon=elm_icon_add(win);
-   elm_icon_standard_set(icon,"arrow_left");
-   evas_object_show(icon);
-   
-   bt = elm_button_add(win);
-   elm_button_icon_set(bt,icon);
-   evas_object_size_hint_weight_set(bt,0.0,0.0);
-   //evas_object_size_hint_align_set(bt, -1.0, -1.0);
-   elm_box_pack_end(bx2, bt);
-   evas_object_smart_callback_add(bt,"clicked",back_bt_clicked, NULL);
-   evas_object_show(bt);
-   
-   icon=elm_icon_add(win);
-   //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
-   elm_icon_standard_set(icon,"arrow_right");
-   evas_object_show(icon);
-   
-   bt = elm_button_add(win);
-   elm_button_icon_set(bt,icon);
-   evas_object_size_hint_weight_set(bt,0.0,0.0);
-   //evas_object_size_hint_align_set(bt, -1.0, -1.0);
-   elm_box_pack_end(bx2, bt);
-   evas_object_smart_callback_add(bt,"clicked",forward_bt_clicked, NULL);
-   evas_object_show(bt);
-   
-   icon=elm_icon_add(win);
-   //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
-   elm_icon_standard_set(icon,"clock");
-   evas_object_show(icon);
-   
-   bt = elm_button_add(win);
-   elm_button_icon_set(bt,icon);
-   evas_object_size_hint_weight_set(bt, 0.0, 0.0);
-   //evas_object_size_hint_align_set(bt, -1.0, -1.0);
-   elm_box_pack_end(bx2, bt);
-   evas_object_smart_callback_add(bt,"clicked",reload_bt_clicked, NULL);
-   evas_object_show(bt);
-   
-   icon=elm_icon_add(win);
-   //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
-   elm_icon_standard_set(icon,"close");
-   evas_object_show(icon);
-   
-   bt = elm_button_add(win);
-   elm_button_icon_set(bt,icon);
-   evas_object_size_hint_weight_set(bt, 0.0, 0.0);
-   //evas_object_size_hint_align_set(bt, -1.0, -1.0);
-   elm_box_pack_end(bx2, bt);
-   evas_object_smart_callback_add(bt,"clicked",stop_bt_clicked, NULL);
-   evas_object_show(bt);
-   
-   //Set up address bar
-   Evas_Object *addlayout=elm_layout_add(win);
-   elm_layout_file_set(addlayout,themefile,"addlayout");
-   evas_object_size_hint_weight_set(addlayout, 1.0, 1.0);
-   elm_box_pack_end(bx2,addlayout);
-   evas_object_show(addlayout);
-   
+    bx = elm_box_add(win);
+    evas_object_size_hint_weight_set(bx, 1.0, 1.0);
+    evas_object_size_hint_align_set(bt, -1.0, -1.0);
+    elm_win_resize_object_add(win, bx);
+    evas_object_show(bx);
+    
+    /* add controls box */
+    bx2 = elm_box_add(win);
+    elm_box_horizontal_set(bx2,TRUE);
+    evas_object_size_hint_weight_set(bx2, 1.0, 0.0);
+    elm_box_homogenous_set(bx2,FALSE);
+    elm_box_pack_end(bx,bx2);
+    evas_object_show(bx2);
+    
+    /* add buttons to control box */
+    
+    icon=elm_icon_add(win);
+    elm_icon_standard_set(icon,"arrow_left");
+    evas_object_show(icon);
+    
+    bt = elm_button_add(win);
+    elm_button_icon_set(bt,icon);
+    evas_object_size_hint_weight_set(bt,0.0,0.0);
+    //evas_object_size_hint_align_set(bt, -1.0, -1.0);
+    elm_box_pack_end(bx2, bt);
+    evas_object_smart_callback_add(bt,"clicked",back_bt_clicked, NULL);
+    evas_object_show(bt);
+    
+    icon=elm_icon_add(win);
+    //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
+    elm_icon_standard_set(icon,"arrow_right");
+    evas_object_show(icon);
+    
+    bt = elm_button_add(win);
+    elm_button_icon_set(bt,icon);
+    evas_object_size_hint_weight_set(bt,0.0,0.0);
+    //evas_object_size_hint_align_set(bt, -1.0, -1.0);
+    elm_box_pack_end(bx2, bt);
+    evas_object_smart_callback_add(bt,"clicked",forward_bt_clicked, NULL);
+    evas_object_show(bt);
+    
+    icon=elm_icon_add(win);
+    //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
+    elm_icon_standard_set(icon,"clock");
+    evas_object_show(icon);
+    
+    bt = elm_button_add(win);
+    elm_button_icon_set(bt,icon);
+    evas_object_size_hint_weight_set(bt, 0.0, 0.0);
+    //evas_object_size_hint_align_set(bt, -1.0, -1.0);
+    elm_box_pack_end(bx2, bt);
+    evas_object_smart_callback_add(bt,"clicked",reload_bt_clicked, NULL);
+    evas_object_show(bt);
+    
+    icon=elm_icon_add(win);
+    //elm_icon_file_set(icon,themefile,"elm/icon/arrow_left/default");
+    elm_icon_standard_set(icon,"close");
+    evas_object_show(icon);
+    
+    bt = elm_button_add(win);
+    elm_button_icon_set(bt,icon);
+    evas_object_size_hint_weight_set(bt, 0.0, 0.0);
+    //evas_object_size_hint_align_set(bt, -1.0, -1.0);
+    elm_box_pack_end(bx2, bt);
+    evas_object_smart_callback_add(bt,"clicked",stop_bt_clicked, NULL);
+    evas_object_show(bt);
+    
+    //Set up address bar
+    Evas_Object *addlayout=elm_layout_add(win);
+    elm_layout_file_set(addlayout,themefile,"addlayout");
+    evas_object_size_hint_weight_set(addlayout, 1.0, 1.0);
+    elm_box_pack_end(bx2,addlayout);
+    evas_object_show(addlayout);
+    
     
     
     //Create the embed widget
@@ -308,64 +244,46 @@ elm_main(int argc, char **argv)
     etk_container_add(ETK_CONTAINER(embed),addressentry);
     etk_signal_connect_by_code(ETK_ENTRY_TEXT_ACTIVATED_SIGNAL,ETK_OBJECT(addressentry),ETK_CALLBACK(addressentry_activated),NULL);
     etk_widget_show_all(embed);
-
+    
     
     addressentry_embed = etk_embed_object_get(ETK_EMBED(embed));
     elm_layout_content_set(addlayout,"addlayout/swallow",addressentry_embed);
     evas_object_show(addressentry_embed);
-   
-   /* add scroller that will house main content */
-   
-   mainscroll=elm_scroller_add(win);
-   evas_object_size_hint_weight_set(mainscroll, 1.0, 1.0);
-   evas_object_size_hint_align_set(mainscroll, -1.0, -1.0);
-   elm_box_pack_end(bx,mainscroll);
-   evas_object_event_callback_add(mainscroll,EVAS_CALLBACK_RESIZE,mainscroll_resizing,NULL);
-   evas_object_show(mainscroll);
-   
-   int scrx,scry,scrw,scrh;
-   evas_object_geometry_get(mainscroll,&scrx,&scry,&scrw,&scrh);
-   //elm_scroller_region_show(mainscroll,0, 0,scrw,scrh);
-   
-   /* set up layout */
-   
-   pagelayout=elm_layout_add(win);
-   elm_layout_file_set(pagelayout,themefile,"pagelayout");
-   free(themefile);
-   elm_scroller_content_set(mainscroll,pagelayout);
-   evas_object_show(pagelayout);
-   
-   /* add webkit component to layout */
-   
-   webkitobj = ewk_webview_object_add(evas);
-   
-   elm_layout_content_set(pagelayout,"pagelayout/swallow",webkitobj);
-   
-   ewk_webview_object_load_url(webkitobj, "http://www.openinkpot.org");
-   
-   ewk_callback_load_progress_add(webkitobj,page_loading,NULL);
-   ewk_callback_url_changed_add(webkitobj,page_url_changed,NULL);
-   evas_object_event_callback_add(webkitobj,EVAS_CALLBACK_MOUSE_DOWN,webkitobj_mouse_down,NULL);
-   evas_object_event_callback_add(webkitobj,EVAS_CALLBACK_MOUSE_MOVE,webkitobj_mouse_move,NULL);
-   evas_object_event_callback_add(webkitobj,EVAS_CALLBACK_MOUSE_UP,webkitobj_mouse_up,NULL);
-   evas_object_event_callback_add(webkitobj,EVAS_CALLBACK_KEY_DOWN,webkitobj_key_down,NULL);
-   
-   
-   evas_object_show(webkitobj);
-   
-   
-    Evas_Object *webpage=ewk_webview_object_webpage_get(webkitobj);
-    if(webpage!=NULL)
-        ewk_webpage_object_viewport_size_set(webpage,scrw,scrh);
-   /* show the window */
-   evas_object_show(win);
-
-
-
-   elm_run();
-   elm_shutdown();
-   ewk_shutdown();
-   etk_shutdown();
-   return 0;
-}
+    
+    
+    /* set up layout */
+    
+    pagelayout=elm_layout_add(win);
+    elm_layout_file_set(pagelayout,themefile,"pagelayout");
+    evas_object_size_hint_weight_set(pagelayout, 1.0, 1.0);
+    evas_object_size_hint_align_set(pagelayout, -1.0, -1.0);
+    free(themefile);
+    elm_box_pack_end(bx,pagelayout);
+    
+    evas_object_show(pagelayout);
+    
+    /* add webkit component to layout */
+    
+    webkitobj = ewk_webview_object_add(evas);
+    
+    elm_layout_content_set(pagelayout,"pagelayout/swallow",webkitobj);
+    
+    ewk_webview_object_load_url(webkitobj, "http://www.openinkpot.org");
+    
+    ewk_callback_load_progress_add(webkitobj,page_loading,NULL);
+    ewk_callback_url_changed_add(webkitobj,page_url_changed,NULL);
+    evas_object_event_callback_add(webkitobj,EVAS_CALLBACK_MOUSE_DOWN,webkitobj_mouse_down,NULL);
+    evas_object_show(webkitobj);
+    
+    
+    evas_object_show(win);
+    
+    
+    
+    elm_run();
+    elm_shutdown();
+    ewk_shutdown();
+    etk_shutdown();
+    return 0;
+    }
 ELM_MAIN()
